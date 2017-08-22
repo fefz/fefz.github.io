a70ab9eef0c7c83a7c607fff7d43c51f84978773
swing <imswing@gmail.com>
2017-08-23 00:04:26 +0800

    update travis config

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd ./public
-git init
-git config --global push.default matching
-git config --global user.email "${GITHUB_MAIL}"
-git config --global user.name "${GITHUB_USER}"
-git add --all .
-git commit -m "Auto Builder of ${GITHUB_USER}'s Blog"
-git push --quiet --force https://${GITHUB_TOKEN}@github.com/${GITHUB_USER}/${REPO}.git master
